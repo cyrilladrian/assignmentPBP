57e7cf437d1d496e32807b162543cd57fc555e56
cyrilladrian <cyrill.adrian@ui.ac.id>
2023-09-19 22:57:03 +0700

    updated for the assignment 3

--- assignment2/main/models.py
+++ assignment2/main/models.py
@@ -5,3 +5,4 @@ class Product(models.Model):
     name = models.CharField(max_length = 255)
     amount = models.IntegerField()
     description = models.TextField()
+    price = models.IntegerField()

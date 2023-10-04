118bdde886330002b74e90f204fc2f84268c7728
cyrilladrian <cyrill.adrian@ui.ac.id>
2023-10-04 09:05:28 +0700

    Updated for Assignment 5

--- assignment2/main/forms.py
+++ assignment2/main/forms.py
@@ -1,7 +1,32 @@
 from django.forms import ModelForm
 from main.models import Product
+from django.contrib.auth.forms import UserCreationForm  
+from django import forms
+from django.core.exceptions import ValidationError 
+from django.contrib.auth.models import User   
 
 class ProductForm(ModelForm):
     class Meta:
         model = Product
         fields = ["name", "price", "description", "amount"]
+
+# class CustomUserCreationForm(UserCreationForm):
+#     username = forms.CharField(label='username')
+#     password1 = forms.CharField(label='password1', widget=forms.PasswordInput)  
+#     password2 = forms.CharField(label='password2', widget=forms.PasswordInput)
+
+#     def clean_password2(self):  
+#         password1 = self.cleaned_data['password1']  
+#         password2 = self.cleaned_data['password2']  
+    
+#         if password1 and password2 and password1 != password2:  
+#             raise ValidationError("Password don't match")  
+#         return password2
+    
+#     def save(self, commit = True):  
+#         user = User.objects.create_user(  
+#             self.cleaned_data['username'],    
+#             self.cleaned_data['password1']  
+#         )  
+#         return user  
+
--- assignment2/main/views.py
+++ assignment2/main/views.py
@@ -15,6 +15,7 @@ import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
+
 # Create your views here.
 @login_required(login_url='/login')
 def show_main(request):
@@ -22,7 +23,7 @@ def show_main(request):
     context = {
         'name': request.user.username,
         'class': 'PBP KI',
-        'app_name' : 'Supermarket Product Management',
+        'app_name' : 'InventLaris',
         'products' : products,
         'last_login': request.COOKIES['last_login']
     }
@@ -58,14 +59,14 @@ def show_json_by_id(request, id):
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
 
 def register(request):
-    form = UserCreationForm()
-
+    form = UserCreationForm()  
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Your account has been successfully created!')
             return redirect('main:login')
+        
     context = {'form':form}
     return render(request, 'register.html', context)
 
@@ -90,3 +91,26 @@ def logout_user(request):
     response = HttpResponseRedirect(reverse('main:login'))
     response.delete_cookie('last_login')
     return response
+
+def edit_product(request, id):
+    # Get product by ID
+    product = Product.objects.get(pk = id)
+
+    # Set product as instance of form
+    form = ProductForm(request.POST or None, instance=product)
+
+    if form.is_valid() and request.method == "POST":
+        # Save the form and return to home page
+        form.save()
+        return HttpResponseRedirect(reverse('main:show_main'))
+
+    context = {'form': form, 'ids': id}
+    return render(request, "edit_product.html", context)
+
+def delete_product(request, id):
+    # Get data by ID
+    product = Product.objects.get(pk=id)
+    # Delete data
+    product.delete()
+    # Return to the main page
+    return HttpResponseRedirect(reverse('main:show_main'))
